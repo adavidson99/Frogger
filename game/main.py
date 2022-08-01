63af95eb1f7ea90e8356519af0fefc36088690bd
adavidson99 <99695975+adavidson99@users.noreply.github.com>
2022-08-01 19:27:23 +1200

    Update main.py

--- game/main.py
+++ game/main.py
@@ -5,40 +5,40 @@ from log import Log
 from random import randint
 
 
-def generate_cars(car_list):
+def generate_cars(car_list, level):
     # randomly generate the cars (and trucks) in the game
     if randint(0, 5) == 0:
-        car_list.append(Car(0))
+        car_list.append(Car(0, level))
     if randint(0, 5) == 0:
-        car_list.append(Car(1))
+        car_list.append(Car(1, level))
     if randint(0, 5) == 0:
-        car_list.append(Car(2))
+        car_list.append(Car(2, level))
     if randint(0, 5) == 0:
-        car_list.append(Car(3))
+        car_list.append(Car(3, level))
     if randint(0, 5) == 0:
-        car_list.append(Car(4))
+        car_list.append(Car(4, level))
 
     return car_list
 
 
-def generate_small_logs(log_list):
+def generate_small_logs(log_list, level):
     # randomly generate the small logs in the game
     if randint(0, 1) == 0:
-        log_list.append(Log(0))
+        log_list.append(Log(0, level))
     if randint(0, 1) == 0:
-        log_list.append(Log(4))
+        log_list.append(Log(4, level))
 
     return log_list
 
 
-def generate_large_logs(log_list):
+def generate_large_logs(log_list, level):
     # randomly generate the medium and large logs in the game
     if randint(0, 1) == 0:
-        log_list.append(Log(1))
+        log_list.append(Log(1, level))
     if randint(0, 1) == 0:
-        log_list.append(Log(2))
+        log_list.append(Log(2, level))
     if randint(0, 1) == 0:
-        log_list.append(Log(3))
+        log_list.append(Log(3, level))
 
     return log_list
 
@@ -48,6 +48,7 @@ def main():
     pygame.init()
     background = pygame.image.load("frogger_background.gif")
     counter = 0
+    level = 0
     car_list = []
     log_list = []
     clock = pygame.time.Clock()
@@ -61,13 +62,13 @@ def main():
         clock.tick(60)
 
         if counter % 40 == 0:
-            car_list = generate_cars(car_list)
+            car_list = generate_cars(car_list, level)
 
         if counter % 100 == 0:
-            log_list = generate_small_logs(log_list)
+            log_list = generate_small_logs(log_list, level)
 
         if counter % 180 == 0:
-            log_list = generate_large_logs(log_list)
+            log_list = generate_large_logs(log_list, level)
 
         for car in car_list:
             car.shift()
@@ -79,6 +80,10 @@ def main():
             player.check_drown(log_list)
 
         player.check_bounds()  # Checks if player is out of bounds
+
+        if player.check_home():  # Checks if player is at the end of the map
+            level += 1
+            player.restart()
 
         counter += 1
 
