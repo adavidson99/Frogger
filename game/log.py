5241b3f7d39a383c3982fb4a401b7a4fff119aee
adavidson99 <99695975+adavidson99@users.noreply.github.com>
2022-08-01 19:28:40 +1200

    Update log.py

--- game/log.py
+++ game/log.py
@@ -3,15 +3,21 @@ import pygame
 
 class Log(pygame.sprite.Sprite):
     # Log class
-    def __init__(self, number=None):
+    def __init__(self, number, level):
         super().__init__()
 
         self.number = number
+        self.speedup = (level * 20)
+        # level 0 = 0% increase
+        # level 1 = 20% increase
+        # level 2 = 40% increase
+        # level 3 = 60% increase...
 
         # images of the objects
         self.objects = ['small_log.png', 'medium_log.png', 'long_log.png', 'medium_log.png', 'small_log.png']
         self.pos = [70, 105, 140, 175, 210]
-        self.speed = [-1.8, 1.6, -1.2, 1.4, -1.6]
+        self.speed = [-1.8+(-1.8/100*self.speedup), 1.6+(1.6/100*self.speedup), -1.2+(-1.2/100*self.speedup),
+                      1.4+(1.4/100*self.speedup), -1.6+(-1.6/100*self.speedup)]
 
         # loading image
         self.image = pygame.image.load(self.objects[number])
